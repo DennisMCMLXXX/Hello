4261f566ea2810e5b097a741e807c97a4a5b7f82
Nicklas Holmberg <45942181+nishog04@users.noreply.github.com>
2019-01-29 12:15:24 +0100

    Returboll
    Sjukt att det blir så fruktansvärt mycket mer när jag gör en ändring...

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Hello
 Hello Java
 Och Tjena till skolprojektet i arbetsmetodik!
+
+
+Tjoflöjt alla glada!
+Här kommer en uppdatering från Nicklas.
+/N
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello Java!\nGreeting from Jkpg");
+        System.out.println("Hello Java!\nGreeting from Jkpg\n");
+        System.out.println("Det är kallt som fan i Skellefteå - och i javavärlden!\nNicklas den okunnige");
     }
 }
